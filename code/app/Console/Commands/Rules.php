6c3dd7fe2a43451697eef52a69f7b26ae47bc855
William Penton <william@penton.us>
2020-11-16 07:04:18 -0600

    added "rules list"
    listing of server rules

--- code/app/Console/Commands/Rules.php
+++ code/app/Console/Commands/Rules.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Discord\DiscordCommandClient;
+use Discord\Parts\Channel\Message;
 use Discord\Parts\User\Member;
 use Illuminate\Console\Command;
 
@@ -47,22 +48,37 @@ class Rules extends Command
             ]);
 
             try {
-                $discordClient->registerCommand('rules', function ($message) use ($discordClient) {
-                    if ($message->channel->name === 'bot-commands') {
-                        if (substr($message->content, 7) === strtolower('agree')) {
-                            $guild = $discordClient->guilds->get('id', env('DISCORD_GUILD_ID'));
-                            $guild->members->fetch($message->author->id)
-                                ->done(function (Member $member) use ($message) {
-                                    $member->addRole("775042190761525299")->done(function () use ($message) {
-                                        $message->reply('welcome!');
-                                    }, function ($e) use ($message) {
-                                        $message->reply("you're already a member.");
+                $discordClient->registerCommand('rules', function (Message $message) use ($discordClient) {
+                    $command = strtolower(substr($message->content, 7));
+                    switch ($command) {
+                        case 'agree':
+                            if ($message->channel->name === 'bot-commands') {
+                                $guild = $discordClient->guilds->get('id', env('DISCORD_GUILD_ID'));
+                                $guild->members->fetch($message->author->id)
+                                    ->done(function (Member $member) use ($message) {
+                                        $member->addRole("775042190761525299")->done(function () use ($message) {
+                                            $message->reply('welcome!');
+                                        }, function ($e) use ($message) {
+                                            $message->reply("you're already a member.");
+                                        });
                                     });
-                                });
-                        } else {
+                            }
+                            break;
+                        case 'list':
+                            $guild = $discordClient->guilds->get('id', env('DISCORD_GUILD_ID'));
+                            $response = "**Rules:**".PHP_EOL.PHP_EOL.
+                                "**1)** Follow Discord TOS".PHP_EOL.
+                                "**2)** I believe in the 1st amendment. Don't be stupid and post anything that violates laws! No doxxing, making threats of physical harm or anything that will make me need to speak with law enforcement.".PHP_EOL.
+                                "**3)** At least try to get along, but if you can't/won't I will turn this server around and make you regret it!".PHP_EOL.
+                                "**4)** Again, DON'T BE A RETARD!".PHP_EOL.
+                                "**5)** Agree to these terms in <#{$guild->channels->get('name', 'bot-commands')->id}> by typing `.rules agree`";
+                            $message->reply($response);
+                            break;
+                        default:
                             $message->reply('you need to "agree".');
-                        }
+                            break;
                     }
+
                 });
             } catch (\Exception $exception) {
                 print_r($exception->getMessage());
